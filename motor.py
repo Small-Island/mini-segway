be43b51f49c5b18e5acaeb017ca348309fdc033e
Small-Island <ojima.yukiya@gmail.com>
2024-07-02 14:42:52 +0900

    update

--- motor.py
+++ motor.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import serial
 import threading
+import socket
 
 setVelM1 = np.array(0, dtype='int16')
 setVelM2 = np.array(0, dtype='int16')
@@ -13,11 +14,37 @@ run = True
 #forward_vel = 0.0 # -1.0 ~ 1.0 (meter/sec)
 
 def udp_loop():
+    global run, setVelM1, setVelM2, udpRecvTime
+    M_SIZE = 16
+    host = "0.0.0.0"
+    port = 9001
+    sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.settimeout(1)
+    print('create socket')
+    sock.bind((host, port))
     # global setVelM1, setVelM2, momoRecvTime
     # ser = serial.Serial('./serial_out', 9600)
     while run:
+        try :
+            print('udp loop: Waiting message')
+            message, cli_addr = sock.recvfrom(M_SIZE)
+            udpRecvTime = time.time()
+            print(f'udp loop: Received message is [{message}]')
+            np_array = np.array([message[0], message[1]], dtype='int8')
+            turning_vel = int(np_array[0])/127 # -127 ~ 127
+            forward_vel = int(np_array[1])/127 # -127 ~ 127
+            setVelM1_int32 = 150 * forward_vel - 50*turning_vel
+            setVelM2_int32 = -(150 * forward_vel + 50*turning_vel)
+            setVelM1 = np.array(setVelM1_int32, dtype='int16')
+            setVelM2 = np.array(setVelM2_int32, dtype='int16')
+        except socket.error as e:
+            print(f"udp loop: {e}")
+        except KeyboardInterrupt :
+            run = False
+            break
+    sock.close()
+        # print('recvVel', setVelM1, setVelM2)
         # print("hello")
-        x = 1
         # data = ser.read(4)
         # momoRecvTime = time.time()
         # read_array = np.array([data[0], data[1], data[2], data[3]], dtype='int8')
@@ -39,50 +66,46 @@ thread_udp.start()
 
 import hid
 h = hid.device()
-while True:
+while run:
     try:
+        time.sleep(1)
         h.open(0x2886, 0x802f)
     except OSError as e:
-        print(e)
-        print("not open. try to open in 1sec")
-        time.sleep(1)
+        print(f"hid loop: {e}")
+        print("hid loop: usb not open. try to open in 1sec")
+        continue
+    except KeyboardInterrupt :
+        run = False
+        thread_udp.join()
+        # quit()
+        break
     else:
         break
 
-print("start")
-while 1:
-    try:
-        if time.time() - momoRecvTime > 1:
-             setVelM1 = 0
-             setVelM2 = 0   
-        time.sleep(0.05)
-        dataBytes = [0, 0, 0, 0, 0]
-        dataBytes[4] = setVelM2 & 0xff
-        dataBytes[3] = (setVelM2 >> 8) & 0xff
-        dataBytes[2] = setVelM1 & 0xff
-        dataBytes[1] = (setVelM1 >> 8) & 0xff
-        h.write(dataBytes)
-        getBytes = h.read(4)
-        getVelM1 = np.array((getBytes[0] << 8) + getBytes[1], dtype='int16') 
-        getVelM2 = np.array((getBytes[2] << 8) + getBytes[3], dtype='int16')
-        print('setVel', setVelM1, setVelM2, "V_M1" , getVelM1, "V_M2", getVelM2)
-        
-    except KeyboardInterrupt :
-        h.close()
-        thread_momo.join()
-        run = False
-        break
-    except Exception as e:
-        print(e)
-        print("hid unknown error")
-        h.close()
-        while True:
-            try:
-                h.close()
-                h.open(0x2886, 0x802f)
-            except OSError as e:
-                print("not open. try to open in 1sec")
-                time.sleep(1)
-            else:
-                break
-        pass
+    print("start")
+    while run:
+        try:
+            if time.time() - momoRecvTime > 1:
+                setVelM1 = 0
+                setVelM2 = 0   
+            time.sleep(0.05)
+            dataBytes = [0, 0, 0, 0, 0]
+            dataBytes[4] = setVelM2 & 0xff
+            dataBytes[3] = (setVelM2 >> 8) & 0xff
+            dataBytes[2] = setVelM1 & 0xff
+            dataBytes[1] = (setVelM1 >> 8) & 0xff
+            h.write(dataBytes)
+            getBytes = h.read(4)
+            getVelM1 = np.array((getBytes[0] << 8) + getBytes[1], dtype='int16') 
+            getVelM2 = np.array((getBytes[2] << 8) + getBytes[3], dtype='int16')
+            print('setVel', setVelM1, setVelM2, "V_M1" , getVelM1, "V_M2", getVelM2)
+            
+        except KeyboardInterrupt :
+            run = False
+            h.close()
+            thread_udp.join()
+            break
+        except Exception as e:
+            print(e)
+            print("hid unknown error")
+            h.close()
--- xbox.py
+++ xbox.py
@@ -4,28 +4,50 @@ import sys, os
 # os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame
+import numpy as np
+import time
+
+turning_vel = 0
+forward_vel = 0
 
 def joy_event(joy):
-
-    hat = joy.get_hat(0) # Get status of cross key
-
-    print(f"Get status of cross key {hat}")
-    axis = (joy.get_axis(0), joy.get_axis(1)) # 左スティックの左右．-1が左
-    print(f"Get status of left axis (左右，上下) = {axis}")
-    axis = (joy.get_axis(2), joy.get_axis(3)) # 左スティックの左右．-1が左
+    global turning_vel, forward_vel
+    # axis = (joy.get_axis(0), joy.get_axis(1)) # 左スティックの左右．
+    # print(f"Get status of left axis (左右，上下) = {axis}")
+    axis = (joy.get_axis(2), joy.get_axis(3)) # 右スティックの左右．
     print(f"Get status of right axis (左右，上下) = {axis}")
-    # if hat[0] == 0 and hat[1] == 1: # Up key pressed
-    #     print("up")
-    # elif hat[0] == 0 and hat[1] == -1: # Down key pressed
-    #    print("down")
-    # elif hat[0] == -1 and hat[1] == 0: # Left key pressed
-    #    print("left")
-    # elif hat[0] == 1 and hat[1] == 0: # Right key pressed
-    #    print("right")
+    turning_vel = int(axis[0]*127)
+    forward_vel = int(axis[1]*127)
 
+run = True
+
+def udp_loop():
+    global run, turning_vel, forward_vel
+    import socket
+    M_SIZE = 1024
+    host = "127.0.0.1"
+    port = 9001
+    sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+    print('create socket')
+    sock.bind((host, port))
+    while run:
+        try :
+            print(f'udp loop: sending ({turning_vel, forward_vel})')
+            sock.sendto(turning_vel.to_bytes(1, 'little', signed=True) + forward_vel.to_bytes(1, 'little', signed=True), (host, port))
+            time.sleep(0.05)
+        except socket.error as e:
+            print(f"udp loop: {e}")
+        except KeyboardInterrupt :
+            run = False
+            break
+    sock.close()
+
+import threading      
+thread_udp = threading.Thread(target=udp_loop)
+thread_udp.start()    
 
 def main():
-
+    global run
     pygame.init()
 
     try:
@@ -44,6 +66,7 @@ def main():
 
             btn_b = joy.get_button(1) # B button to exit
             if btn_b == 1:
+                run = False
                 break
 
             print("To quit, press B-button.")
